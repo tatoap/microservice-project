fd442516f5b2335df1ac89ea2b2fa20ff4729461
Renato <tato_ap@hotmail.com>
2021-06-24 23:09:16 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/developer/hrpayroll/entity/Worker.java
+++ hr-payroll/src/main/java/com/developer/hrpayroll/entity/Worker.java
@@ -1,30 +1,18 @@
-package com.developer.hrworker.entity;
+package com.developer.hrpayroll.entity;
 
 import java.io.Serializable;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "worker")
 public class Worker implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	private String name;
-	
 	private Double dailyIncome;
 	
 	public Worker() {
--- hr-payroll/src/main/java/com/developer/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/developer/hrpayroll/service/PaymentService.java
@@ -1,14 +1,34 @@
 package com.developer.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.developer.hrpayroll.entity.Payment;
+import com.developer.hrpayroll.entity.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId.toString());
+						
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+		
+		//return new Payment("Bob", 200.0, days);
 	}
 
 }
