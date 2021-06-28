b0ab11c29b54be0f7499fcfdaf579872d6d75be4
Renato <tato_ap@hotmail.com>
2021-06-28 17:40:20 -0300

    UserFeignClient

--- hr-oauth/src/main/java/com/developer/hroauth/entity/User.java
+++ hr-oauth/src/main/java/com/developer/hroauth/entity/User.java
@@ -1,18 +1,24 @@
 package com.developer.hroauth.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -33,6 +39,38 @@ public class User implements Serializable {
 		this.email = email;
 		this.password = password;
 		this.roles = roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> 
+			new SimpleGrantedAuthority(role.getRoleName()))
+			.collect(Collectors.toList());
+	}
+	
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
